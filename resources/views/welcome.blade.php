c258c1cc4240ec437b790a945f2d68a00ea8ff16
cuongphan87 <cuongphanxuan87@gmail.com>
2018-08-28 14:45:40 +0700

    add laravel text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Begin Learning
                 </div>
 
                 <div class="links">
